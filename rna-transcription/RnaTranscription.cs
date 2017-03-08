d4f886ac85e3d4acd41f7ffac9a9161de99b383d
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-08 19:30:21 -0300

    rna-transcription done

--- rna-transcription/RnaTranscription.cs
+++ rna-transcription/RnaTranscription.cs
@@ -4,6 +4,29 @@ public static class Complement
 {
     public static string OfDna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+
+        String changed = "";
+        for (int i = 0; i < nucleotide.Length; i++)
+        {
+            if (nucleotide[i] == 'G')
+            {
+                changed += "C";
+            }
+            else if (nucleotide[i] == 'C')
+            {
+                changed += "G";
+            }
+            else if (nucleotide[i] == 'T')
+            {
+                changed += "A";
+            }
+            else
+            {
+                changed += "U";
+            }
+        }
+
+        return changed;
     }
 }
