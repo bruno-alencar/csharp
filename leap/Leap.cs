7b16530284cc48ceff541e578b83e1c19edab7c6
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-08 16:35:02 -0300

    Leap test done

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -4,6 +4,20 @@ public static class Year
 {
     public static bool IsLeap(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+
+      bool validator = false; 
+        if (year % 4 == 0)
+        {
+            validator = true;
+            if (year % 100 == 0)
+            {
+                validator = false;
+                if (year % 400 == 0)
+                    validator = true;
+            }
+        }
+
+        return validator;
     }
 }
