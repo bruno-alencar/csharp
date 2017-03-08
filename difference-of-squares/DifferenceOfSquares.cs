a4fc3f769a5aee154b5f5919cbf3283eb363922e
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-08 18:37:56 -0300

    Difference-of-squares

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -4,16 +4,34 @@ public static class Squares
 {
     public static int SquareOfSums(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+        
+        int valor = 0;
+        for(int i = 1; i <= max; i ++)
+        {
+            valor += i;
+        }
+
+        return valor * valor;
     }
 
     public static int SumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+        
+        int valor = 0;
+        for(int i = 1; i <= max; i++)
+        {
+            valor += i * i;
+        }
+
+        return valor;
     }
 
     public static int DifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+
+        return SquareOfSums(max) - SumOfSquares(max);
     }
 }
