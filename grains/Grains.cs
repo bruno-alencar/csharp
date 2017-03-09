ec468008adc343dc95e771248f376a51822860ec
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-09 11:04:17 -0300

    grains test done

---
+++ grains/Grains.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        // throw new NotImplementedException("You need to implement this function.");
+        ulong valor = 0;
+        for(int i = 1 ; i <= n; i ++)
+        {
+            if(valor == 0)
+                valor = 1;
+            else
+                valor = valor*2;
+        }
+
+        return valor;
+    }
+
+    public static ulong Total()
+    {
+        // throw new NotImplementedException("You need to implement this function.");
+        ulong valor = 0;
+        ulong total = 0;
+        for(int i = 1 ; i <= 64; i ++)
+        {
+            if(valor == 0)
+                valor = 1;
+            else
+                valor = valor*2;
+
+            total += valor;
+        }
+
+        return total;
+    }
+}
