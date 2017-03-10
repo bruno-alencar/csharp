edb4746b0d02f0e99498b5abae840c70e3eb1ca4
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-10 12:51:27 -0300

    proverb test done;

---
+++ proverb/Proverb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Proverb
+{
+    public static string Line(int line)
+    {
+        // throw new NotImplementedException();
+
+        List<string> allFullPhrase = new List<string>();
+
+        allFullPhrase.Add("For want of a nail the shoe was lost.");
+        allFullPhrase.Add("For want of a shoe the horse was lost.");
+        allFullPhrase.Add("For want of a horse the rider was lost.");
+        allFullPhrase.Add("For want of a rider the message was lost.");
+        allFullPhrase.Add("For want of a message the battle was lost.");
+        allFullPhrase.Add("For want of a battle the kingdom was lost.");
+        allFullPhrase.Add("And all for the want of a horseshoe nail.");
+
+        return allFullPhrase[line-1];
+    }
+
+    public static string AllLines()
+    {
+          List<string> allFullPhrase = new List<string>();
+
+        allFullPhrase.Add("For want of a nail the shoe was lost.");
+        allFullPhrase.Add("For want of a shoe the horse was lost.");
+        allFullPhrase.Add("For want of a horse the rider was lost.");
+        allFullPhrase.Add("For want of a rider the message was lost.");
+        allFullPhrase.Add("For want of a message the battle was lost.");
+        allFullPhrase.Add("For want of a battle the kingdom was lost.");
+        allFullPhrase.Add("And all for the want of a horseshoe nail.");
+
+        string results = "";
+        foreach(string phrase in allFullPhrase)
+            results += phrase +"\n";
+        
+        results = results.Substring(0, results.Length-1);
+
+        return results;
+    }
+}
