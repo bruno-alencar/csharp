fc0ca60ca1f01497efaae9a7d578299297fd25e1
Bruno Alencar <bruno.idealencar@gmail.com>
2017-03-13 12:17:33 -0300

    perfect-numbers test done;

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum NumberType
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+        // throw new NotImplementedException();
+        int sum = 0;
+
+        for (int i = 1; i < number; i++)
+        {
+            if(number % i == 0)
+                sum += i;
+        }
+
+        if(sum == number)
+            return NumberType.Perfect;
+        else if(sum > number)
+            return NumberType.Abundant;
+        else
+            return NumberType.Deficient;
+
+    }
+}
